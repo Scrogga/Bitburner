a733d35ce38becc11fec7814e7fb1f7fe9021967
Scrogga <scroggiealex@gmail.com>
2023-06-08 21:23:11 +0800

    Updates

--- purchase-servers.js
+++ purchase-servers.js
@@ -3,11 +3,27 @@ export async function main(ns) {
     const ram = 8;
     let i = 0;
 
+    const red = "\u001b[31m";
+    const cyan = "\u001b[36m";
+
+    var target = 'joesguns';
+    if (ns.args.length > 0) {
+        target = ns.args[0];
+    }
+
     while (i < ns.getPurchasedServerLimit()) {
         if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(ram)) {
             let hostname = ns.purchaseServer('pserv-' + i + '-' + ram + 'GB', ram);
-            ns.scp('early-hack-joe.js', hostname);
-            ns.exec('early-hack-joe.js', hostname, 3);
+            ns.tprint('Server has low ram, defaulting to less efficient hack script on ' + hostname);
+            ns.tprint('Initialising on ' + hostname);
+            ns.scp('/Hack/init.js', hostname);
+            ns.exec('/Hack/init.js', hostname, 1, target);
+            await ns.sleep(100)
+            let ramAvailable = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname);
+            let ramPerThread = ns.getScriptRam('/Hack/hack-old.js');
+            let threads = Math.floor(ramAvailable / ramPerThread);
+            ns.scp('/Hack/hack-old.js', hostname);
+            ns.exec('/Hack/hack-old.js', hostname, threads, target);
             i++;
         }
         await ns.sleep(1000);
--- server-info.js
+++ server-info.js
@@ -13,17 +13,26 @@ export async function main(ns) {
         ns.tprint('Requires a target. Exiting.');
         ns.exit();
     }
+    
+    var hackExecutionTime = (Math.floor((ns.getHackTime(target)/1000/60) << 0) + '.' + Math.floor((ns.getHackTime(target)/1000) % 1000) + 'm');
+    var growExecutionTime = (Math.floor((ns.getGrowTime(target)/1000/60) << 0) + '.' + Math.floor((ns.getGrowTime(target)/1000) % 1000) + 'm');
+    var weakenExecutionTime = (Math.floor((ns.getWeakenTime(target)/1000/60) << 0) + '.' + Math.floor((ns.getWeakenTime(target)/1000) % 1000) + 'm');
+    var currentSecurityLevel = ns.getServerSecurityLevel(target);
+    var currentServerMoney = ns.getServerMoneyAvailable(target);
+    var hackChance = ns.hackAnalyzeChance(target) * 100;
+    hackChance = hackChance.toFixed(2)
+    
 
     ns.tprint(green + 'Target: ' + cyan + target);
     ns.tprint(green + 'Root access: ' + cyan + ns.hasRootAccess(target));
-    ns.tprint(green + 'Hack() execution time: ' + cyan + ns.getHackTime(target));
-    ns.tprint(green + 'Grow() execution time: ' + cyan + ns.getGrowTime(target));
-    ns.tprint(green + 'Weaken() execution time: ' + cyan + ns.getWeakenTime(target));
+    ns.tprint(green + 'Hack() execution time: ' + cyan + hackExecutionTime);
+    ns.tprint(green + 'Grow() execution time: ' + cyan + growExecutionTime);
+    ns.tprint(green + 'Weaken() execution time: ' + cyan + weakenExecutionTime);
     ns.tprint(green + 'Server growth rate: ' + cyan + ns.getServerGrowth(target));
     ns.tprint(green + 'Minimum security level: ' + cyan + ns.getServerMinSecurityLevel(target));
-    ns.tprint(green + 'Current security level: ' + cyan + ns.getServerSecurityLevel(target));
+    ns.tprint(green + 'Current security level: ' + cyan + currentSecurityLevel.toFixed(2));
     ns.tprint(green + 'Maximum server money: ' + cyan + ns.getServerMaxMoney(target));
-    ns.tprint(green + 'Current server money: ' + cyan + ns.getServerMoneyAvailable(target));
-    ns.tprint(green + 'Chance to hack: ' + cyan + ns.hackAnalyzeChance(target));
+    ns.tprint(green + 'Current server money: ' + cyan + currentServerMoney.toFixed(0));
+    ns.tprint(green + 'Chance to hack: ' + cyan + hackChance + '%');
     //ns.tprint('' + ns.);
 }
